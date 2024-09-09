6c56af1ad41b4ee3f41a8c8cc569eb48ba863575
linnsheng <linnsheng2002@gmail.com>
2024-09-09 10:43:24 +0800

    Daily

---
+++ 2326.Speed_Matrix_IV.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <vector>
+
+/*
+Given that the dimension of matrix (m * n)
+Goal: Generate an m x n matrix
+*/
+class Solution {
+public:
+    std::vector<std::vector<int>> spiralMatrix(int m, int n, ListNode* head) 
+    {
+        
+    }
+};
