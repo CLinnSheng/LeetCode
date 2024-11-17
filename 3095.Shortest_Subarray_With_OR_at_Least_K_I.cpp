a687223a38b6384c91b35f056d92e08b7665c713
linnsheng <linnsheng2002@gmail.com>
2024-11-17 16:33:03 +0800

    Daily

--- 3095.Shortest_Subarray_With_OR_at_Least_K_I.cpp
+++ 3095.Shortest_Subarray_With_OR_at_Least_K_I.cpp
@@ -10,45 +10,70 @@
  * greater than the maximum element in the subarray. So we can use a sliding
  * window when it reach equal or greater than k then we try to move the left
  * window to find the minimum length
+ * Constraint: The elements come be a negative number, so we can solve this by
+ * having a prefix and we use prioty queue to store the prefix Time Complexity:
+ * O(nlgn) because of push & pop operation of heap Space Complexity: O(n)
+ * Actually there is a more clever way, since we want to remove the prefix sum
+ * that is the longest & with minSum
+ * By using minHeap, it will automatically help us to get the min how about we
+ * manually do it ourself we can use a monotomic increasing stack
  *
- * Time Complexity: O(32 * n)
- * Space Complexity: O(1)
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
  * */
+
+using ll = long long;
+
+// struct comparator {
+//   bool operator()(std::pair<ll, int> &a, std::pair<ll, int> &b) {
+//     return a.first > b.first;
+//   }
+// };
 class Solution {
 public:
-  int minimumSubarrayLength(std::vector<int> &nums, int k) {
+  int shortestSubarray(std::vector<int> &nums, int k) {
     int ans{INT_MAX};
-    std::vector<int> bits(32, 0);
+    ll currSum{};
 
-    int currSum{};
+    int right{};
 
-    std::function<void(const int &, int)> updateBits = [&](const int &action,
-                                                           int val) {
-      for (int i{}; i < 32; i++)
-        if ((val >> i) & 1)
-          bits[i] += action;
-    };
+    // std::priority_queue<std::pair<ll, int>, std::vector<std::pair<ll, int>>,
+    //                     comparator>
+    //     minHeap;
+    //
+    // while (right < nums.size()) {
+    //   currSum += nums[right];
+    //
+    //   // dealing with base case only 1 elements & nt revmoing any prefix
+    //   if (currSum >= k)
+    //     ans = std::min(ans, right + 1);
+    //
+    //   while (!minHeap.empty() && currSum - minHeap.top().first >= k) {
+    //     ans = std::min(ans, right - minHeap.top().second);
+    //     minHeap.pop();
+    //   }
+    //
+    //   minHeap.emplace(currSum, right++);
+    // }
+    std::deque<std::pair<ll, int>> stack;
 
-    std::function<int()> checkValue = [&]() {
-      int sum{};
-      for (int i{}; i < 32; i++)
-        if (bits[i])
-          sum |= 1 << i;
-      return sum;
-    };
-
-    int left{}, right{};
     while (right < nums.size()) {
-      updateBits(1, nums[right]);
+      currSum += nums[right];
 
-      // moving the left ptr if already get at least k
-      while (left <= right && checkValue() >= k) {
-        ans = std::min(ans, right - left + 1);
-        // moving the left ptr
-        updateBits(-1, nums[left++]);
+      if (currSum >= k)
+        ans = std::min(ans, right + 1);
+
+      // finding the minimum valid window
+      while (!stack.empty() && currSum - stack.front().first >= k) {
+        ans = std::min(ans, right - stack.front().second);
+        stack.pop_front();
       }
 
-      right++;
+      // making sure is a monotomic stack
+      while (!stack.empty() && stack.back().first >= currSum)
+        stack.pop_back();
+
+      stack.emplace_back(currSum, right++);
     }
     return ans == INT_MAX ? -1 : ans;
   }
