d4fe46aba7a0ad0b74e64e357c9e80b1d1bb719c
linnsheng <linnsheng2002@gmail.com>
2025-02-06 19:40:34 +0800

    Daily

---
+++ 1726.Tuplw_With_Same_Product.cpp
@@ -0,0 +1,44 @@
+#include <unordered_map>
+#include <vector>
+using std::vector;
+
+/*
+ * Given an array nums of distinct integers, return the number of tuples (a, b, c, d) such that a * b = c * d and they
+ * are all not equal
+ *
+ * Intuition:
+ * from the example we know that the tuple (a, b, c, d) has 8 diff combinations
+ * where
+ * a * b = c * d
+ * a * b = d * c
+ * b * a = c * d
+ * b * a = d * c
+ * and another 4 just switching the left and right
+ *
+ * so basically we just need to found out all possible product from the nums.
+ * Time Complexity: O(n^ 2)
+ * */
+class Solution
+{
+  public:
+    int tupleSameProduct(vector<int> &nums)
+    {
+        if (nums.size() < 4)
+            return 0;
+
+        std::unordered_map<int, int> product_freq;
+        for (int i{}; i < nums.size(); i++)
+            for (int j{}; j < nums.size(); j++)
+                if (i != j)
+                    product_freq[nums[i] * nums[j]]++;
+
+        int answer{};
+
+        for (const auto &map : product_freq)
+        {
+            int pairs{map.second * ((map.second - 1) / 2)};
+            answer += 8 * pairs;
+        }
+        return answer;
+    }
+};
