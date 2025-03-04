5009c85b1ca69257a9bb794fba0acd96324c60fa
linnsheng <linnsheng2002@gmail.com>
2025-03-05 00:57:36 +0800

    Daily

--- 2161.Partition_Array_According_to_Given_Pivot.cpp
+++ 2161.Partition_Array_According_to_Given_Pivot.cpp
@@ -1,10 +1,35 @@
 #include <vector>
 using std::vector;
 
+/*
+ * Goal: Return the nums after the arrangement
+ *
+ * Intuition:
+ * Since the order of the arrangement doesnt matter
+ * What we need to do is just partition the number into 3 part lesser, equal and greater
+ * Then in the end just append all together
+ * Time Complexity: O(n)
+ * */
 class Solution
 {
   public:
     vector<int> pivotArray(vector<int> &nums, int pivot)
     {
+        vector<int> lesser, equal, greater;
+
+        for (const auto &num : nums)
+        {
+            if (num == pivot)
+                equal.emplace_back(num);
+            else if (num > pivot)
+                greater.emplace_back(num);
+            else
+                lesser.emplace_back(num);
+        }
+
+        lesser.insert(lesser.end(), equal.begin(), equal.end());
+        lesser.insert(lesser.end(), greater.begin(), greater.end());
+
+        return lesser;
     }
 };
