588e30bba8f9037535644b123a27defc589c535e
linnsheng <linnsheng2002@gmail.com>
2025-04-22 10:15:12 +0800

    neetcode 150

--- String_Encode_and_Decode.cpp
+++ String_Encode_and_Decode.cpp
@@ -1,46 +1,57 @@
-#include <bits/stdc++.h>
 #include <string>
-using namespace std;
+#include <strings.h>
+#include <vector>
 
 /*
-Goal: Design an algorithm for both encoding and decoding string
-Given that strs[i] contains only UTF-8 characters
-eg: ':', '#', '$'
-Basically all symbols, characters & alphabets
+ * Goal: Design an algorithm to encode a list of strings to a single string.
+ * Encoded string is then decoded back to the original list of strings.
+ * Constraints: strs[i] contians only UTF-8 characters
+ *
+ * Intuition:
+ * We need to encode the list of string into 1 string. How can we do it to indicate which letter ends at where at the
+ * entire string? We can track the length of each string and encoded it into it, but there is one problem strs[i]
+ * contians all UTF-8 characters means that even number and special character as well. So maybe we can do is len then +
+ * '#' as a sign of starting character after the '#'
+ * Encoded format: len#strlen#str
+ * Decoded format: extract the len, then we know how many character for each string after the #.
+ *
+ * */
+class Solution
+{
+  public:
+    std::string encode(std::vector<std::string> &strs)
+    {
+        std::string encode_str;
 
-Intuition: Since we want to keep track of every single string, so it best to have include the length of every single string
-But the constraint is the string itself might contain letters in the front
-The solution for it will be create a unique identity, so that we know how many characters for it
-and when to start reading it
+        for (int index{}; index < strs.size(); index++)
+        {
+            int len(strs[index].length());
 
-    string encode(vector<string>& strs) {
-
-        string encode_string = "";
-        
-        for (const auto& str : strs)
-            encode_string += to_string(str.length()) + "#" + str;
-        
-        return encode_string;
+            encode_str += std::to_string(len) + "#" + strs[index];
+        }
+        return encode_str;
     }
 
-    vector<string> decode(string s) {
+    std::vector<std::string> decode(std::string s)
+    {
+        std::vector<std::string> decode_str;
 
-        vector<string> decode_strings;
-        
-        for (int i = 0; i < s.length();) {
-        
-            // Remember the length of each string is also in the form of str and it might contains more than 1 character
-            int end_length = i;
+        for (int index{}; index < s.length(); index++)
+        {
+            int end_index{index};
 
-            while (s[end_length] != '#')
-                end_length++;
-                
-            int length = stoi(s.substr(i, end_length - i));
-            
-            decode_strings.emplace_back(s.substr(end_length + 1, length));
-            i = end_length + 1 + length;
+            // extract the len
+            while (s[end_index] != '#')
+                end_index++;
+
+            int len(std::stoi(s.substr(index, end_index - index)));
+
+            index = end_index + 1;
+            end_index += len;
+
+            decode_str.emplace_back(s.substr(index, end_index - index + 1));
+            index = end_index;
         }
-        
-        return decode_strings;
+        return decode_str;
     }
 };
