9a488ebf9a529606196fa503478b3d9acd6d7832
linnsheng <linnsheng2002@gmail.com>
2025-04-25 08:03:14 +0800

    neetcode 150

--- 169.Majority_Element.cpp
+++ 169.Majority_Element.cpp
@@ -1,45 +1,37 @@
-#include <unordered_map>
 #include <vector>
 
 /*
- * Given an array nums of size, return the majority element
- * Majority element is the element that appears more than n / 2 times.
+ * Goal: Find the majority in the element that appears more than [n/2] times
  *
  * Intuition:
- * Just use hash data structure to keep track of the count
+ * The most straight away approach is by using hash map
+ * Store it in the hash map and iterate the hash map again to find out whose has n/2 times
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  *
- * Can we do it in O(1) of space complexity?
- * Just 2 variable to keep track of the freq & the current most element
+ * But there's a much more clever way which will only take O(1) space
+ * If we observe there is only one elment which is majority.
+ * So if we count from the to the right where the count is current count of the majority element.
+ * We will decrement it it whenever it is not equal to the current majority element. So in just simply says the majority
+ * element will be surviving at last
  * */
 class Solution
 {
   public:
     int majorityElement(std::vector<int> &nums)
     {
-        int n(nums.size());
-        // std::unordered_map<int, int> freq_count;
-        //
-        // for (const auto &num : nums)
-        //     freq_count[num]++;
-        //
-        // for (const auto &[val, freq] : freq_count)
-        //     if (freq >= n / 2.0)
-        //         return val;
-        //
-        // return -1;
+        int freq{}, ans{};
 
-        // > 0 is 1 or < 0 is 0
-        int freq{}, currentMostElement{};
         for (const auto &num : nums)
         {
-            // when equal to zero switch to new element
             if (freq == 0)
-                currentMostElement = num;
+            {
+                ans = num;
+            }
 
-            freq += currentMostElement == num ? 1 : -1;
+            freq += ans == num ? 1 : -1;
         }
-        return currentMostElement;
+
+        return ans;
     }
 };
