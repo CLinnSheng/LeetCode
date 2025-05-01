f5c230022c4ac52b4e55cdad8460eb69e1bf0715
linnsheng <linnsheng2002@gmail.com>
2025-05-01 13:58:56 +0800

    neetcode

--- 304.Range_Sum_Query_2D_Immutable.cpp
+++ 304.Range_Sum_Query_2D_Immutable.cpp
@@ -1,62 +1,66 @@
 #include <vector>
-using std::vector;
 
 /*
- * Goal: Implement the NumMatrix Class
- * The constructor initializes the object with the integer matrix.
- * sumRegion function return the sum of elements of matrix inside the rectangle defined by its
- * upper left corner and lower right corner.
+ * Goal: Return the sum of the elements of matrix inside the rectangle defined by the upper left corner and lower right
+ * corner.
  *
+ * Intuition:
+ * The brute force way will just simply iterate throuh every single elment within the rectangle
+ * But this will take O(n^2)
  *
+ * How can we optimize it to O(1) for SumRegion??
+ * The way only we is to use prefixSum. For the normal prefixSum is llike prefixSum[i] means the sum from 0 to ith.
+ * But how do we design it such that it can use for 2d matrix?
+ * The O(n) way will is take the prefix sum of the col2 - sum of the the col1 and iterate through the array between row1
+ * and row2. How do we make it O(1)?
+ *
+ * We can design our prefix sum such that prefixSum[i][j] will be the sum of all the elements before ith and jth.
+ * eg: prefixSum[1][1] will be the sum of all element nums[0][0], nums[0][1], nums[1][0], nums[1][1]
+ * and then how do we compute the sum of the particular rectangle
+ * the fomrula is prefixsum of bottom right - prefixsum bottom left -1 - prefixsum top right - 1 + prefixsum top left -
+ * 1
  * */
 class NumMatrix
 {
-  public:
+  private:
     std::vector<std::vector<int>> prefixSum;
-    NumMatrix(vector<vector<int>> &matrix)
-    {
-        int ROWS(matrix.size()), COLS(matrix[0].size());
-        prefixSum = std::vector<std::vector<int>>(ROWS + 1, std::vector<int>(COLS + 1, 0));
 
-        for (int row{}; row < ROWS; row++)
+  public:
+    NumMatrix(std::vector<std::vector<int>> &matrix)
+    {
+        int ROW(matrix.size()), COL(matrix[0].size());
+        // need to offset 1 because of row 0 and col 0
+        prefixSum = std::vector<std::vector<int>>(ROW + 1, std::vector<int>(COL + 1, 0));
+
+        for (int i{}; i < ROW; i++)
         {
             int sum{};
-            for (int col{}; col < COLS; col++)
+
+            for (int j{}; j < COL; j++)
             {
-                sum += matrix[row][col];
-                int above{prefixSum[row][col + 1]};
-                prefixSum[row + 1][col + 1] = sum + above;
+                sum += matrix[i][j];
+
+                // remeber the prefixSum is offset by 1
+                int above{prefixSum[i][j + 1]};
+                prefixSum[i + 1][j + 1] = sum + above;
             }
         }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2)
     {
-        // just iterate every single row & col within the rectangle
-        // this is the brute force way. Time Complexity: O(m*n)
-        // How can we make it in O(1)? the only way is to precopmute the sum (prefix) at every index
-        // but just finding the prefix of a row still couldnt achieve O(1) [actual time complexity: O(m)]
-        // the way to compute the prefix is we find the row 1 & also add the above this will make the prefix of the
-        // rectangle
-        // int sum{};
-        //
-        // for (int i{row1}; i <= row2; i++)
-        //     for (int j{col1}; j <= col2; j++)
-        //         sum += matrix[i][j];
-        //
-        // return sum;
-
-        // fix the offset first
+        // REMEMBER OFF SET BY1
         row1++;
         col1++;
         row2++;
         col2++;
-        int bottomRight = prefixSum[row2][col2];
-        int topRight = prefixSum[row1 - 1][col2];
-        int bottomLeft = prefixSum[row2][col1 - 1];
 
-        // we add because we minus the area twice
-        return bottomRight - topRight - bottomLeft + prefixSum[row1 - 1][col1 - 1];
+        int bottomRight{prefixSum[row2][col2]};
+        int bottomLeft{prefixSum[row2][col1 - 1]};
+        int topRight{prefixSum[row1 - 1][col2]};
+
+        // add back the area got minus twice
+        return bottomRight - bottomLeft - topRight + prefixSum[row1 - 1][col1 - 1];
     }
 };
 
