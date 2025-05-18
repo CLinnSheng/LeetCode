5b12b1501676c449662994014135f9f466d293d3
linnsheng <linnsheng2002@gmail.com>
2025-05-19 00:03:13 +0800

    150 neetcode

--- 189.Rotate_Array.cpp
+++ 189.Rotate_Array.cpp
@@ -1,28 +1,30 @@
-#include <unordered_map>
 #include <utility>
 #include <vector>
-using std::vector;
 
 /*
- * Goal: Rotate the array to the right by k steps.
+ * Goal: Rotate the array to the right by k steps where k is non negative
  *
- * Intuition:
- * Use an additional array to store the element with new index
+ * IntuitioN:
+ * The array is rotated to the right.
+ * k can be any numbers of from [0, 100000]
+ * So instead of looping k we can at most actually rotate the array by n - 1
  * Time Complexity: O(n)
  * Space Complexity: O(n)
  *
- * How do we do it in O(1) space
- * Through the observation of rotating the array k times, we notice there are 2 groups of element in the array
- * THe last k element will be shift to the front & then rest of the element will be shift to the end.
- * We can achieve it by first reverse all the element. Then we will reverse the element from 0 until k - 1 in order to
- * follow the order Then the same for k until n -1 element eg: 1, 2, 3, 4, 5, 6, 7 -> 3 reversed: 7, 6, 5, 4, 3, 2, 1
- * (0, k - 1) 5, 6, 7, 4, 3, 2, 1
- * (k, n - 1) 5, 6, 7, 1, 2, 3, 4
+ * Can we optimize the space complexity? to O(1)??
+ * So that means we cant use extra space. The most we can use is use variable
+ * If we really go and observer how the rotate works, we will see there are 2 groups of element in the array
+ * the left group will be move to the end and the right group will move to the front after k reverse
+ * What we can do is reverse the 2 groups first then only reverse the internal group
+ * eg: 1,2,3,4,5,6,7 & k = 3 The 2 groups will be 1,2,3,4 & 5,6,7
+ * 1st reverse: 7,6,5,4,3,2,1
+ * 2nd reverse: 5,6,7,4,3,2,1
+ * 3rd reverse: 5,6,7,1,2,3,4
  * */
 class Solution
 {
   private:
-    void reverse(int left, int right, vector<int> &nums)
+    void reverse(std::vector<int> &nums, int left, int right)
     {
         while (left < right)
         {
@@ -33,25 +35,24 @@ class Solution
     }
 
   public:
-    void rotate(vector<int> &nums, int k)
+    void rotate(std::vector<int> &nums, int k)
     {
         int n(nums.size());
-        if (n == k)
-            return;
-
+        // finding out the true k
         k %= n;
 
-        // // O(n) Space
-        // // val -> index
-        // std::vector<int> temp(n);
+        // Approach 1: Space Complexity of O(n)
+        // std::vector<int> temp(nums.begin(), nums.end());
+        //
         // for (int i{}; i < n; i++)
         //     temp[(i + k) % n] = nums[i];
         //
         // for (int i{}; i < n; i++)
         //     nums[i] = temp[i];
 
-        reverse(0, n - 1, nums);
-        reverse(0, k - 1, nums);
-        reverse(k, n - 1, nums);
+        // Approach 2: Space Complexity: O(1)
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
     }
 };
