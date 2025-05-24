61b83b4061fd16f0949760d576a336294ef4b827
linnsheng <linnsheng2002@gmail.com>
2025-05-24 12:14:24 +0800

    150 neetcode

--- 11.Container_With_Most_Water.cpp
+++ 11.Container_With_Most_Water.cpp
@@ -1,34 +1,41 @@
 #include <algorithm>
-#include <climits>
-#include <ios>
-#include <iostream>
 #include <vector>
 
 /*
-Given an array of height
-Goal: Return the container with most water which is the maximum area
-Instuition: Can use 2 pointer and try to find the maximum area by moving the pointer with shorter height
+ * There are n vertical lines
+ * Goal: Return the maximum amount of water a container can store
+ *
+ * Intuition:
+ * The naive way will just simply have 2 nested loop that will try all possible sizes of container.
+ * But this will take O(n^2)
+ *
+ * How can we optimize it? To O(n) or others?
+ * Area = Length * Height
+ * Since we cannot control the length but we can control the height.
+ * We also need to be greedy at the same time.
+ * We can use 2 pointer approach, every time we will move the pointer with the lower heights
+ * Time Complexity: O(N)
+ * */
+class Solution
+{
+  public:
+    int maxArea(std::vector<int> &height)
+    {
 
-        
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        int size = height.size();
-        
-        int start = 0, end = size - 1;
-        
-        int max_area = INT_MIN;
-        
-        while (start < end) {
-            
-            max_area = std::max(max_area, std::min(height[start], height[end]) * (end - start));
-            
-            if (height[start] > height[end]) end--;
-            else start++;
-        
+        int maxArea{};
+        int left{}, right(height.size() - 1);
+
+        while (left < right)
+        {
+            int area{std::min(height[left], height[right]) * (right - left)};
+            maxArea = std::max(area, maxArea);
+
+            if (height[left] > height[right])
+                right--;
+            else
+                left++;
         }
-        
-        return max_area;
+
+        return maxArea;
     }
 };
