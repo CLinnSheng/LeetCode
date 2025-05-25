46026338b325ff0ebb179592ad431f3f56d35ee2
linnsheng <linnsheng2002@gmail.com>
2025-05-25 23:10:14 +0800

    150 neetcode

--- 121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,35 +1,44 @@
-#include <ios>
-#include <iostream>
 #include <vector>
 
 /*
-Goal: Return the maxium profit
-Given an array of prices for n day where prices[i] is the price of the given stock on the ith day
-Intuition: Selling price must be higher than the buying price, can use a sliding window algo
-keep expanding the window if selling price is higher than buying price & only shift the buying days when we occur selling < buying that means we can buy at that day because it has lwoer price
-*/
-class Solution {
-public:
-    int maxProfit(std::vector<int>& prices) {
-        
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        int left = 0, right = left + 1;
-        int maxProfit = 0;
-        int days = prices.size();
-        
-        while (right < days) {
-            
+ * prices[i] is the price on ith day.
+ * Choose a single day to buy and choose another day to sell
+ * Goal: Return the maximum profit you can achieve.
+ *
+ * Intuition:
+ * We only can choose 1 day to buy and another day in the future to sell.
+ * The brute force will just simply trying all possible combination of days to buy and sell
+ * But this will take O(N^2)
+ * How can we optimiz it?
+ * Since the days is important we cannot just simply sort it.
+ * There is one algo we can make use of is sliding window.
+ * So we will maintain the window to find the best buy and selling price.
+ * We will keep moving the right pointer as long as the sell day is lesser than buy day.
+ * Only move the left pointer if and only if the buy price is greater than the sell day. (Means that we found a cheap
+ * buy day)
+ *
+ * Time Complexity: O(n)
+ * */
+class Solution
+{
+  public:
+    int maxProfit(std::vector<int> &prices)
+    {
+        int left{}, right{1};
+        int days(prices.size());
+
+        int maxProfit{};
+
+        while (right < days)
+        {
             if (prices[left] < prices[right])
                 maxProfit = std::max(maxProfit, prices[right] - prices[left]);
             else
                 left = right;
-                
+
             right++;
         }
-        
+
         return maxProfit;
     }
 };
--- 219.Contains_Duplicate_II.cpp
+++ 219.Contains_Duplicate_II.cpp
@@ -1,37 +1,26 @@
-#include <cstdlib>
 #include <unordered_map>
 #include <vector>
-using std::vector;
-
 /*
- * Goal: Return true if there are 2 distinct indices i & j such that nums[i] == nums[j] and abs(i - j) <= k
+ * Goal: Return true if there are 2 distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <=
+ * k
  *
  * Intuition:
- * The first come to mind is using sliding window because we try to find within a specific range then need to keep
- * shifting it Just keep moving the right pointer until more than k than only move the left pointer
- * But there's a little bit complicated to code it out
- *
- * The best way is we try to cache the index of every single element, so we can just access it in O(1) for the index of
- * element we want
- * We can use hash data structure
+ * The information we need is the value and its index
+ * So we can make use hash map
  * */
+
 class Solution
 {
   public:
-    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    bool containsNearbyDuplicate(std::vector<int> &nums, int k)
     {
-        int size(nums.size());
-        if (size == 1)
-            return false;
+        std::unordered_map<int, int> map;
 
-        std::unordered_map<int, int> hashIndex;
-        for (int i{}; i < size; i++)
-        {
-            if (hashIndex.count(nums[i]))
-                if (std::abs(i - hashIndex[nums[i]]) <= k)
-                    return true;
-            hashIndex[nums[i]] = i;
-        }
+        for (int i{}; i < nums.size(); i++)
+            if (map.count(nums[i]) && i - map[nums[i]] <= k)
+                return true;
+            else
+                map[nums[i]] = i;
         return false;
     }
 };
