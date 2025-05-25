5a5808500daebe84a6e6581777346e8ce3de3aad
linnsheng <linnsheng2002@gmail.com>
2025-05-25 15:51:40 +0800

    150 neetcode

--- 42.Trapping_Rain_Water.cpp
+++ 42.Trapping_Rain_Water.cpp
@@ -1,41 +1,45 @@
-#include <ios>
-#include <iostream>
+#include <algorithm>
 #include <vector>
 
 /*
-Goal: Compute how much water it can trap
-Intuition: To compute the trapped water we need to know the height from each side adn then min(left, right) - height[i] --> water trap in that column
-we can use 2 arrays to store every single position maxHeight from the left and the right
-and then the amount of water trapped is just simply min(leftMax, rightMax) - height[i]
-Instead of having 2 arrays we can use 2 pointers, 1 in the left and 1 in the right
-both of them is responsible to compute the tracp water from each direction
-Time Complexity: O(N)
+ * Given n non-negative integer representing an elevation map where the width of each bar is 1.
+ * Goal: Compute how much water it can trap.
+ *
+ * Intuition:
+ * One important key observation is the water trap is not just based on its left & right boundary.
+ * Is actually based on the left max & right max.
+ * for eg: 2,1,0,1,2
+ * The water stored at the index having 0 is not 1 but actually 2.
+ * So what we can do is have a prefix and suffix array that store the maximum left and right for each index.
+ * Another approach without using any additional space is actually using 2 pointer.
+ * Since we want to find the total water and we assume the water has to be maximum, so we will keep
+ * moving the pointer with lesser heigher
+ * Since we moving the pointer with lesser height, we only need to consider its side as the other side is definitely
+ * greater than it Time Complexity: O(n)
+ * */
+class Solution
+{
+  public:
+    int trap(std::vector<int> &height)
+    {
+        int waters{};
+        int left{}, right(height.size() - 1);
+        int leftMax{height[left]}, rightMax{height[right]};
 
-        
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        std::cout.tie(nullptr);
-        
-        
-        int left = 0, right = height.size() - 1;
-        int leftMax = height[left], rightMax = height[right];
-        int ttl_water = 0;
-        
-        while (left < right) {
-            
-            if (leftMax < rightMax) {
-                // Keep updating the maximum height from the LHS & then compute every single index trapped water
+        while (left < right)
+        {
+            if (leftMax < rightMax)
+            {
                 leftMax = std::max(leftMax, height[++left]);
-                ttl_water += leftMax - height[left];
+                waters += leftMax - height[left];
             }
-            
-            else {
+            else
+            {
                 rightMax = std::max(rightMax, height[--right]);
-                ttl_water += rightMax - height[right];
+                waters += rightMax - height[right];
             }
-            
         }
-        
-        return ttl_water;
+
+        return waters;
     }
 };
