b8e48ebd7531e34788e49c05463e974689080e65
linnsheng <linnsheng2002@gmail.com>
2025-08-01 23:05:55 +0800

    neetcode

--- 297.Serialize_and_Deserialize_Binary_Tree.cpp
+++ 297.Serialize_and_Deserialize_Binary_Tree.cpp
@@ -6,62 +6,93 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 
 /*
-Goal: Serialize and deserialize the binary tree
-Intuition: Serialize the binary tree through the way we traverse the tree
-At here, we traverse the tree through preorder traversal because we want to
-build the tree from the top as the first node in preorder is the root node we
-use a delimiter to seperate between each node in the string
+ * Serialize: Process of converting an in-memory structure into a sequence of bits
+ * Task: Ensure binary tree can be serialize into a string and can be deserialized to the original tree structure.
+ *
+ * Intuition:
+ * The treeNode is in level order traversal. How do we serialize into a string while still can handle nullptr
+ *
+ * */
+#include <functional>
+#include <sstream>
+#include <string>
+#include <vector>
+class Codec
+{
+  private:
+    std::string join(const std::vector<std::string> &str, const std::string &delimiter)
+    {
+        std::string answer{};
 
-For deserialize we just follow the way we traverse through the tree
+        for (const auto &s : str)
+        {
+            if (&s != &str[0])
+                answer += delimiter;
 
-Time Complexity: O(n)
-Space Complexity: O(lgn)
-*/
-class Codec {
-public:
-  // Encodes a tree to a single string.
-  string serialize(TreeNode *root) {
-    std::stringstream s;
-    dfs_serialize(root, s);
-    return s.str();
-  }
-
-  // Decodes your encoded data to tree.
-  TreeNode *deserialize(string data) {
-    std::stringstream input(data);
-    return dfs_deserialize(input);
-  }
-
-  // Store it preorderly because is easier to track which nodes is leaf node
-  // the last node that company by two nullptr will be the leaf node
-  void dfs_serialize(TreeNode *root, std::stringstream &s) {
-    if (!root)
-      s << "N" << ' ';
-    else {
-      // for stringstream by default is seperate by whitespace, so we use
-      // whitespace to seperate it
-      s << root->val << ' ';
-      dfs_serialize(root->left, s);
-      dfs_serialize(root->right, s);
+            answer += s;
+        }
+        return answer;
     }
-  }
 
-  TreeNode *dfs_deserialize(std::stringstream &input) {
-    std::string current_val;
-    input >> current_val;
-    if (current_val == "N")
-      return nullptr;
+    void split(std::vector<std::string> &answer, const std::string &data, const char &delimiter)
+    {
+        std::string token;
+        std::istringstream tokenStream(data);
 
-    TreeNode *node = new TreeNode(stoi(current_val));
-    node->left = dfs_deserialize(input);
-    node->right = dfs_deserialize(input);
+        while (std::getline(tokenStream, token, delimiter))
+            answer.push_back(token);
+    }
 
-    return node;
-  }
+  public:
+    // Encodes a tree to a single string.
+    // a leaf can just represent with 2 nullptr follow by it
+    // Preorder Traversal
+    string serialize(TreeNode *root)
+    {
+        std::vector<std::string> answer;
+        std::function<void(TreeNode *)> serializeHelperDFS = [&](TreeNode *node) {
+            if (node == nullptr)
+            {
+                answer.emplace_back("N");
+                return;
+            }
+
+            answer.emplace_back(std::to_string(node->val));
+            serializeHelperDFS(node->left);
+            serializeHelperDFS(node->right);
+        };
+
+        serializeHelperDFS(root);
+        return join(answer, ",");
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode *deserialize(string data)
+    {
+        std::vector<std::string> answer;
+        split(answer, data, ',');
+        int index{};
+
+        std::function<TreeNode *(int &)> deserializeHelperDFS = [&](int &index) -> TreeNode * {
+            // Base Case
+            if (answer[index] == "N")
+            {
+                index++;
+                return nullptr;
+            }
+
+            TreeNode *node = new TreeNode(std::stoi(answer[index++]));
+            node->left = deserializeHelperDFS(index);
+            node->right = deserializeHelperDFS(index);
+
+            return node;
+        };
+
+        return deserializeHelperDFS(index);
+    }
 };
