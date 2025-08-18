f8c6c9dbc17d26be237bf55ba9831dfc81a68f15
linnsheng <linnsheng2002@gmail.com>
2025-08-19 00:34:35 +0800

    neetcode

--- 684.Redundant_Connection.cpp
+++ 684.Redundant_Connection.cpp
@@ -1,44 +1,48 @@
-/*
-Goal: Return a edge that can be removed so that the resulting graph is a tree
-Note: Tree has no cycles.
-Constraint: Return the occurs last in the input
-
-Intuition: Use UnionFind Algorithm to connect the nodes and check whether theres
-a cycle or not Time Complexity: O(n + n) [1 n is for the initialization of the
-array of parents & the other n is iterate through n edges and also involve
-findParent(this is constant time complexity due to the path compression)] Space
-Complexity: O(n)
-*/
-#include <functional>
 #include <vector>
 
-class Solution {
-public:
-  std::vector<int>
-  findRedundantConnection(std::vector<std::vector<int>> &edges) {
+/*
+ * You are given a connected undirected graph with n nodes labeled from 1 to n. Initially, it contained no cycles and
+ * consisted of n-1 edges.
+ * Goal: Return an edge that can be remove to make the graph dont have cycle.
+ *
+ * Intuition:
+ * So the qeustion is telling us the edge is added 1 by 1 and no cycle initially. Then eventually there will beo ne edge
+ * that will form a cycle within the graph. So we need to find out that edge. We can use unionFind algorithm to connect
+ * the nodes. Such that when we find out the 2 nodes share the same parent means that it will form a cycl
+ * */
+class Solution
+{
+  private:
+    int findParent(const int &node, std::vector<int> &parents)
+    {
+        if (parents[node] != node)
+            parents[node] = findParent(parents[parents[node]], parents);
+        return parents[node];
+    }
+    bool unionFind(const int &node1, const int &node2, std::vector<int> &parents)
+    {
+        int parent1{findParent(node1, parents)}, parent2{findParent(node2, parents)};
 
-    std::vector<int> parents(edges.size() + 1);
+        if (parent1 == parent2)
+            return true;
 
-    std::function<int(const int &)> findParent = [&](const int &x) {
-      if (parents[x] != x)
-        parents[x] = findParent(parents[parents[x]]);
-
-      return parents[x];
-    };
-
-    for (int i = 0; i <= edges.size(); i++)
-      parents[i] = i;
-
-    for (const auto &edge : edges) {
-      int parent1{findParent(edge[0])};
-      int parent2{findParent(edge[1])};
-
-      if (parent1 == parent2)
-        return std::vector<int>{edge[0], edge[1]};
-      else
+        // If doesnt form the cycle or in another way share the same parent
+        // then we connected these 2 noeds together
         parents[parent1] = parent2;
+        return false;
     }
 
-    return {};
-  }
+  public:
+    std::vector<int> findRedundantConnection(std::vector<std::vector<int>> &edges)
+    {
+        std::vector<int> parents;
+        // Initially all nodes are its own parent
+        for (int i{}; i <= edges.size(); i++)
+            parents.emplace_back(i);
+
+        for (const auto &edge : edges)
+            if (unionFind(edge[0], edge[1], parents))
+                return {edge[0], edge[1]};
+        return {};
+    }
 };
