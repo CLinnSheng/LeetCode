53ce08f1b530e069c1301d45c19b48caf5b020b5
linnsheng <linnsheng2002@gmail.com>
2025-08-25 17:08:01 +0800

    daily

--- 787.Cheapest_Flight_Withink_K_Stops.cpp
+++ 787.Cheapest_Flight_Withink_K_Stops.cpp
@@ -3,43 +3,48 @@
 
 /*
  * Goal: Find the cheapest path within k moves
+ *
+ * NOTE: k means number of stops so if k = 1, we can traverse 3 nodes which mean the middle 1 is the stop
+ *
  * Intuition: At first thought, im thinking of using dijkstra algorithm but it
  * has to do some modifications on it because we got a constraint we can only
  * has k stop. So it will not be so efficient. THen im thinking of switching to
- * bellman ford algorithm which will be much more efficient Time Compleixty:
- * O(E*k) Space Compleixty: O(V)
+ * bellman ford algorithm which will be much more efficient
+ * Is a bit similar to BFS.
+ * Time Complexity: O(E * K)
  * */
-class Solution {
-public:
-  int findCheapestPrice(int n, std::vector<std::vector<int>> &flights, int src,
-                        int dst, int k) {
+class Solution
+{
+  public:
+    int findCheapestPrice(int n, std::vector<std::vector<int>> &flights, int src, int dst, int k)
+    {
+        std::vector<int> prices(n, INT_MAX);
+        prices[src] = 0;
 
-    std::vector<int> prices(n, INT_MAX);
-    prices[src] = 0;
+        for (int i{}; i <= k; i++)
+        {
+            // Make a copy of the prices & make changes on the copy 1
+            // because we traversing every single edge.
+            std::vector<int> temp(prices.begin(), prices.end());
 
-    // iterate k + 1 times because the number of stops is the number of nodes
-    // between start and end noe (not inclusive)
-    for (int i = 0; i <= k; i++) {
-      // have a temp prices because of the 'k' stops if not we might move more
-      // than 1 step at once
-      std::vector<int> temp(prices.begin(), prices.end());
+            for (const auto &flight : flights)
+            {
+                auto from{flight[0]}, to{flight[1]}, price{flight[2]};
 
-      for (const auto &flight : flights) {
-        int s = flight[0];
-        int d = flight[1];
-        int w = flight[2];
+                // Only BFS on the visited node.
+                if (prices[from] == INT_MAX)
+                    continue;
 
-        // only move the node we hav reached
-        if (prices[s] == INT_MAX)
-          continue;
+                auto flightPrice{prices[from] + price};
+                if (temp[to] > flightPrice)
+                {
+                    temp[to] = flightPrice;
+                }
+            }
 
-        int cost = prices[s] + w;
-        if (cost < temp[d])
-          temp[d] = cost;
-      }
+            prices = std::move(temp);
+        }
 
-      prices = std::move(temp);
+        return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
-    return prices[dst] == INT_MAX ? -1 : prices[dst];
-  }
 };
