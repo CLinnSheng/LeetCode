3a8d5c92b9cc0fc0ea5d4c203331ffc60611b5a6
linnsheng <linnsheng2002@gmail.com>
2025-09-01 13:53:57 +0800

    daily

--- 1792.Maximum_Average_Pass_Ratio.cpp
+++ 1792.Maximum_Average_Pass_Ratio.cpp
@@ -1,76 +1,64 @@
 #include <queue>
 #include <utility>
 #include <vector>
-
+using std::vector, std::priority_queue;
 /*
- * Given a 2d array classes where classes[i] = [passi, totali]. We know
- * beforehand that in ith classes there are totali total students but only passi
- * number of students pass the exam
+ * classes[i] = [pass_i, total_i], another extraStudents that guaranteed pass the exam
+ * Assign each of the extraStudents to a class in a way to maximize the average pass ratio across the class
+ * Goal: Return the average pass ratio
  *
- * Another integer extraStudents where extraStudentss number of students that
- * are guaranteed to pass the exam. Assign each of the extraStudents students to
- * a class in a way that maximimzes the average pass ratio across all the
- * classes
- *
- * Goal: Return the maximum possible average pass ratio after assigning the
- * extraStudents students. Answer within 10^-5 of the actual naswer will be
- * accepted
- *
- * Intuition: We need to choose the class that add in the new student will
- * greatly increase the average pass ratio So every time we will be choosing the
- * 1 that increase the average pass ratio the most So we can use a maxHeap data
- * structure to pop out the 1 with the most great change
+ * Intuition:
+ * So we trying to assign the extraStudent to maximize the pass ratio across the class. So we will assign to classes
+ * that have lower pass ratio. Then how can we easily access classes with lower pass ratio? We can make use of heap to
+ * easily access the class with low pass ratio.
+ * One important thing that adding to lowest class with pass ratio doesnt mean will increase the average pass ratio, so
+ * instead of determine through the pass ratio. We use increase in ratio
+ * Time Complexity: O(nlgn)
  *
  * */
-
-struct comparator {
-  bool operator()(const std::pair<double, std::pair<int, int>> &a,
-                  const std::pair<double, std::pair<int, int>> &b) {
-    return a.first < b.first;
-  }
+struct comparator
+{
+    template <typename T> bool operator()(const T &A, const T &B)
+    {
+        return A.first < B.first;
+    }
 };
+class Solution
+{
+  public:
+    double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
+    {
+        std::priority_queue<std::pair<double, std::pair<int, int>>, std::vector<std::pair<double, std::pair<int, int>>>,
+                            comparator>
+            maxHeap;
 
-class Solution {
-public:
-  double maxAverageRatio(std::vector<std::vector<int>> &classes,
-                         int extraStudents) {
+        auto findChange = [&](const auto &pass, const auto &ttlStudent) {
+            return (((double)pass + 1) / (ttlStudent + 1)) - ((double)pass / ttlStudent);
+        };
 
-    int n_class(classes.size());
-    if (n_class == 1)
-      return (double)classes[0][0] / classes[0][1];
+        for (const auto &class_ : classes)
+            maxHeap.emplace(findChange(class_[0], class_[1]), std::make_pair(class_[0], class_[1]));
 
-    std::priority_queue<std::pair<double, std::pair<int, int>>,
-                        std::vector<std::pair<double, std::pair<int, int>>>,
-                        comparator>
-        maxHeap;
+        while (extraStudents)
+        {
+            auto top{maxHeap.top()};
+            maxHeap.pop();
 
-    auto findChange = [](const int &passes, const int &total) {
-      return ((double)(passes + 1) / (total + 1) - (double)passes / total);
-    };
+            maxHeap.emplace(findChange(top.second.first + 1, top.second.second + 1),
+                            std::make_pair(top.second.first + 1, top.second.second + 1));
 
-    for (const auto &class_ : classes)
-      maxHeap.emplace(std::make_pair(findChange(class_[0], class_[1]),
-                                     std::make_pair(class_[0], class_[1])));
+            extraStudents--;
+        }
 
-    while (extraStudents) {
-      auto [changePassRatio, classInfo] = maxHeap.top();
-      maxHeap.pop();
+        double sumRatio{};
+        while (!maxHeap.empty())
+        {
+            auto top{maxHeap.top()};
+            maxHeap.pop();
 
-      maxHeap.emplace(std::make_pair(
-          findChange(classInfo.first + 1, classInfo.second + 1),
-          std::make_pair(classInfo.first + 1, classInfo.second + 1)));
-      extraStudents--;
+            sumRatio += (double)top.second.first / top.second.second;
+        }
+
+        return sumRatio / classes.size();
     }
-
-    double ans{};
-
-    while (!maxHeap.empty()) {
-      auto [_, classInfo] = maxHeap.top();
-      maxHeap.pop();
-
-      ans += (double)classInfo.first / classInfo.second;
-    }
-
-    return ans / n_class;
-  }
 };
