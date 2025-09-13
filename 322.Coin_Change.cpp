d46e0c7af8619746d48f952b71c41ca2495292c6
linnsheng <linnsheng2002@gmail.com>
2025-09-13 20:29:03 +0800

    neetcode

--- 322.Coin_Change.cpp
+++ 322.Coin_Change.cpp
@@ -20,16 +20,23 @@ class Solution
   public:
     int coinChange(std::vector<int> &coins, int amount)
     {
+        if (amount == 0)
+            return 0;
+
         std::vector<int> dp(amount + 1, INT_MAX);
         dp[0] = 0;
 
-        // bottom up approach
-        for (int i{1}; i <= amount; i++)
-            // try every single coin, to see which 1 is the minimum
-            for (const auto &coin : coins)
-                if (i >= coin && dp[i - coin] != INT_MAX)
-                    dp[i] = std::min(dp[i], dp[i - coin] + 1);
-
+        for (int currAmt{1}; currAmt <= amount; currAmt++)
+        {
+            for (const auto coin : coins)
+            {
+                // Only choose the coin if currAmt is greater than the coin and the currAmt - coin got computed before
+                if (currAmt >= coin && dp[currAmt - coin] != INT_MAX)
+                {
+                    dp[currAmt] = std::min(1 + dp[currAmt - coin], dp[currAmt]);
+                }
+            }
+        }
         return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
