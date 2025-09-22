f31415edce2d78a14205abc5ff6a4b7a9aa74957
linnsheng <linnsheng2002@gmail.com>
2025-09-22 23:48:00 +0800

    neetcode

--- 55.Jump_Game.cpp
+++ 55.Jump_Game.cpp
@@ -1,35 +1,30 @@
-#include <ios>
-#include <iostream>
 #include <vector>
 
 /*
  * Given an integer array nums. Each element in the array represents your
- * maximum jump length at that position Goal: Return true if able to reach the
- * last index
+ * maximum jump length at that position
+ * Goal: Return true if able to reach the last index
  *
  * Intuition:
- * 1. Naive approahc by using double loop which is very costly
+ * 1. Naive approach by keep trying from [1, nums[i]] and each index
  * 2. Second way is more clever since we want to know are we able to reach the
  * final index or not why note we just straight away start from final index - 1,
  * check from final index - 1 are we able to reach the last index (keep
  * updating) This will reduce the time complexity to O(n) Space complexity: O(1)
  * */
-class Soltuion {
-public:
-  bool canJump(std::vector<int> &nums) {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+class Solution
+{
+  public:
+    bool canJump(std::vector<int> &nums)
+    {
+        int goal(nums.size() - 1);
 
-    int len = nums.size();
-    if (len == 0)
-      return true;
+        for (int index(nums.size() - 2); index >= 0; index--)
+            // Check whether the current index + nums[i] can reach the previous index or not
+            if (index + nums[index] >= goal)
+                goal = index;
 
-    int last_index = len - 1;
-    for (int i = len - 2; i >= 0; i--) {
-      if (nums[i] > last_index - index)
-        last_index = i;
+        // Check whether can we reach index 0
+        return goal == 0;
     }
-    return last_index == 0;
-  }
 };
