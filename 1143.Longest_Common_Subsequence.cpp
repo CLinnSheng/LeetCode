430e76e54dcb21dae2d1458a2d8d05ebe106bc71
linnsheng <linnsheng2002@gmail.com>
2025-10-24 23:19:19 +0800

    daily

--- 1143.Longest_Common_Subsequence.cpp
+++ 1143.Longest_Common_Subsequence.cpp
@@ -1,37 +1,55 @@
+#include <algorithm>
 #include <string>
 #include <vector>
-using std::string;
 
 /*
+ * Subsequence ca be obtain by deleting some or no elements without changing the relative order of the remaining
+ * character
  * Goal: Return the longest common subsequence
  *
  * Intuition:
- * First thought is this a decision tree problem because at each index i can choose to include it or not
- * So this lead me to think of i can solve it by using dp?
- * Yes, dp[i][j] stands for text1[0-i] & text2[0-j] longest common subsequence
- * So when text1[i] == text2[j] we just need to take the previous index longest common subsequence and add by 1
- * If they are not equal, we need to choose whether we want to skip the current character of text1 or text2
- * Time Complexity: O(n^2)
+ * At each step, we have 2 choices
+ * 1. If character of the current index at both string are equal, then we move both indices forward and extend the
+ * subsequence
+ * 2. If not equal, we can explore both path by advancing one index at a time and recursively finding the longest
+ * subsequence, then choose the one with the maximum subsequent length
+ * Time Complexity: O(2^(m + n))
+ *
+ * Observe from the brute force way, we can actually we doing some repetitive computation
+ * Therefor we can optimize it by using caching. And the information we passing down from the recursion is actually just
+ * the indices of both string. So the caching gonna be 2d array.
+ * Where caching[i][j] --> Longest subsequent from index i and index j
+ * Time Complexity: O(m*n)
  * */
 class Solution
 {
-  public:
-    int longestCommonSubsequence(string text1, string text2)
+  private:
+    int dfs(const std::string &text1, const std::string &text2, int i, int j, std::vector<std::vector<int>> &caching)
     {
-        int len1(text1.length()), len2(text2.length());
-        std::vector<std::vector<int>> dp(len1 + 1, std::vector<int>(len2 + 1, 0));
+        // Base Case
+        if (i == text1.length() || j == text2.length())
+            return 0;
 
-        for (int i{1}; i <= len1; i++)
-            for (int j{1}; j <= len2; j++)
-            {
-                // equal character then we just take the previous index longest common subsequence and add 1 to it
-                if (text1[i - 1] == text2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    // not equal then we need to decide whether to skip this character of text 1 or text 2
-                    dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
-            }
+        // Check Caching
+        if (caching[i][j] != -1)
+        {
+            return caching[i][j];
+        }
+        // 2 Path
+        // If same then move 2 indices
+        if (text1[i] == text2[j])
+        {
+            return caching[i][j] = 1 + dfs(text1, text2, i + 1, j + 1, caching);
+        }
 
-        return dp[len1][len2];
+        // If not the same take the path that leads to longer subsequence
+        return caching[i][j] = std::max(dfs(text1, text2, i + 1, j, caching), dfs(text1, text2, i, j + 1, caching));
+    }
+
+  public:
+    int longestCommonSubsequence(std::string text1, std::string text2)
+    {
+        std::vector<std::vector<int>> caching(text1.length() + 1, std::vector<int>(text2.length() + 1, -1));
+        return dfs(text1, text2, 0, 0, caching);
     }
 };
