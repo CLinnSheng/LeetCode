40dd5f8127b23bac6a07f3f1c9b21865b609d5be
linnsheng <linnsheng2002@gmail.com>
2025-11-02 22:23:36 +0800

    daily

--- 2257.Count_Unguarded_Cells_in_the_Grid.cpp
+++ 2257.Count_Unguarded_Cells_in_the_Grid.cpp
@@ -11,83 +11,85 @@
  * Time Complexity: O(m * n)
  * Space Complexity: O(m * n)
  * */
-class Solution {
-public:
-  const int UNGUARDED{0};
-  const int GUARDED{1};
-  const int GUARD{2};
-  const int WALL{3};
+class Solution
+{
+  public:
+    enum STATE
+    {
+        UNGUARDED = 0,
+        GUARDED,
+        GUARD,
+        WALL
+    };
 
-  int countUnguarded(int m, int n, std::vector<std::vector<int>> &guards,
-                     std::vector<std::vector<int>> &walls) {
+    int countUnguarded(int m, int n, std::vector<std::vector<int>> &guards, std::vector<std::vector<int>> &walls)
+    {
 
-    std::vector<std::vector<int>> map(m, std::vector<int>(n, 0));
+        std::vector<std::vector<int>> map(m, std::vector<int>(n, STATE::UNGUARDED));
 
-    for (const auto &guard : guards)
-      map[guard[0]][guard[1]] = GUARD;
+        for (const auto &guard : guards)
+            map[guard[0]][guard[1]] = STATE::GUARD;
 
-    for (const auto &wall : walls)
-      map[wall[0]][wall[1]] = WALL;
+        for (const auto &wall : walls)
+            map[wall[0]][wall[1]] = STATE::WALL;
 
-    // Go to every row first
-    for (int i{}; i < m; i++) {
-      bool guard = false;
-      // Look from the left
-      for (int j{}; j < n; j++)
-        if (map[i][j] == GUARD)
-          guard = true;
-        else if (map[i][j] == WALL)
-          guard = false;
-        else if (!map[i][j] &&
-                 guard) // only take care of empty cooridnates, for other just
-                        // skip if it stay until the last condition
-          map[i][j] = GUARDED;
-      guard = false;
-      // Look from the right
-      for (int j{m - 1}; j >= 0; j--)
-        if (map[i][j] == GUARD)
-          guard = true;
-        else if (map[i][j] == WALL)
-          guard = false;
-        else if (!map[i][j] &&
-                 guard) // only take care of empty cooridnates, for other just
-                        // skip if it stay until the last condition
-          map[i][j] = GUARDED;
+        // Go to every row first
+        for (int i{}; i < m; i++)
+        {
+            bool guard = false;
+            // Look from the left
+            for (int j{}; j < n; j++)
+                if (map[i][j] == STATE::GUARD)
+                    guard = true;
+                else if (map[i][j] == STATE::WALL)
+                    guard = false;
+                else if (!map[i][j] && guard) // only take care of empty cooridnates, for other just
+                                              // skip if it stay until the last condition
+                    map[i][j] = STATE::GUARDED;
+            guard = false;
+            // Look from the right
+            for (int j{n - 1}; j >= 0; j--)
+                if (map[i][j] == STATE::GUARD)
+                    guard = true;
+                else if (map[i][j] == STATE::WALL)
+                    guard = false;
+                else if (!map[i][j] && guard) // only take care of empty cooridnates, for other just
+                                              // skip if it stay until the last condition
+                    map[i][j] = STATE::GUARDED;
+        }
+
+        // Go to every column
+        for (int j{}; j < n; j++)
+        {
+            bool guard = false;
+            // Look from above
+            for (int i{}; i < m; i++)
+
+                if (map[i][j] == STATE::GUARD)
+                    guard = true;
+                else if (map[i][j] == STATE::WALL)
+                    guard = false;
+                else if (!map[i][j] && guard) // only take care of empty cooridnates, for other just
+                                              // skip if it stay until the last condition
+                    map[i][j] = STATE::GUARDED;
+            guard = false;
+            // Look from bottom
+            for (int i{m - 1}; i >= 0; i--)
+                if (map[i][j] == STATE::GUARD)
+                    guard = true;
+                else if (map[i][j] == STATE::WALL)
+                    guard = false;
+                else if (!map[i][j] && guard) // only take care of empty cooridnates, for other just
+                                              // skip if it stay until the last condition
+                    map[i][j] = STATE::GUARDED;
+        }
+        int ans{};
+
+        for (int i{}; i < m; i++)
+            for (int j{}; j < n; j++)
+                if (map[i][j] == STATE::UNGUARDED)
+                    ans++;
+
+        return ans;
     }
-
-    // Go to every column
-    for (int j{}; j < n; j++) {
-      bool guard = false;
-      // Look from above
-      for (int i{}; i < m; i++)
-
-        if (map[i][j] == GUARD)
-          guard = true;
-        else if (map[i][j] == WALL)
-          guard = false;
-        else if (!map[i][j] &&
-                 guard) // only take care of empty cooridnates, for other just
-                        // skip if it stay until the last condition
-          map[i][j] = GUARDED;
-      guard = false;
-      // Look from bottom
-      for (int i{m - 1}; i >= 0; i--)
-        if (map[i][j] == GUARD)
-          guard = true;
-        else if (map[i][j] == WALL)
-          guard = false;
-        else if (!map[i][j] &&
-                 guard) // only take care of empty cooridnates, for other just
-                        // skip if it stay until the last condition
-          map[i][j] = GUARDED;
-    }
-    int ans{};
-
-    for (int i{}; i < m; i++)
-      for (int j{}; j < n; j++)
-        if (map[i][j] == UNGUARDED)
-          ans++;
-
-    return ans;
-  }
 };
