d45bf1195b5bb173ac3c34919850b5db7810379f
linnsheng <linnsheng2002@gmail.com>
2025-11-03 16:04:27 +0800

    nc

--- 1140.Stone_Game_II.cpp
+++ 1140.Stone_Game_II.cpp
@@ -2,62 +2,14 @@
 using namespace std;
 
 /*
-array piles is a list of piles containing number of stones piles[i]
-goal: end with the most stones for alice
-Alice start first, everytime the player can choose from n piles from previous player to 2n
-intuition: dynamic programming and min_max
-*/
-class Solution {
-    
-    public:
-        int stoneGameII(vector<int>& piles) {
-            
-            ios_base :: sync_with_stdio(false);
-            cin.tie(nullptr);
-            cout.tie(nullptr);
-            
-            int n_piles = piles.size();
-            
-            // this array stores the number of stones available at each position
-            vector<int> suffixSum(n_piles + 1, 0);
-            for (int i = n_piles - 1; i >= 0; i--)
-                suffixSum[i] = suffixSum[i + 1] + piles[i];
-                
-            // memo for dynamic programming
-            // the first dimension is to indicate strating from which index
-            // the second dimension is to indicate the number of piles to take in the previous turn
-            vector<vector<int>> memo(n_piles, vector<int>(n_piles + 1, 0));
-            
-            function<int(int, int)> maxStone = [&](int m, int current_pile) {
-              
-                // out of bound, no more piles to take
-                if (current_pile >= n_piles)
-                    return 0;
-                    
-                // if we can take all piles then take all piles
-                if (current_pile + 2 * m >= n_piles)
-                    return suffixSum[current_pile];
-                    
-                // if we encounter this before, straight away look form the memo
-                if (memo[current_pile][m])
-                    return memo[current_pile][m];
-                    
-                int maxStones = 0;
-                
-                // explore all possible choices through recursive call at each step
-                for (int i = 1; i <= 2 * m; i++) {
-                    // calculate the maximum number of stone alice can get in this turn
-                    // number of stones that avilable from this pile to the last minus with
-                    // what bob will get
-                    int current_stone = suffixSum[current_pile] - maxStone(max(m ,i), current_pile + i);
-                    maxStones = max(current_stone, maxStones);
-                }
-                
-                memo[current_pile][m] = maxStones;
-                return maxStones;
-                    
-            };
-            
-            return maxStone(1, 0);
-        }
+ * Each player can take all the stones in the first X remaining piles, where 1 <= X <= 2M.
+ * Then set M = max(M, X). Initially, M = 1
+ * Goal: maximum stones get
+ */
+class Solution
+{
+  public:
+    int stoneGameII(vector<int> &piles)
+    {
+    }
 };
--- 1325.Delete_Leaves_With_a_Given_Value.cpp
+++ 1325.Delete_Leaves_With_a_Given_Value.cpp
@@ -14,8 +14,12 @@
  * Goal: Delete every node where its value is equal to target.
  *
  * Intuition:
- * We must start to delete from the leaf node & then go upwards.
- * Since need to start from the bottom, then we need to use postorder traversal
+ * We need to delete every leaf node where its value is equal to target. But how can we delete and traverse the tree?
+ * There is 4 ways to traverse, inorder, preorder, postorder, and level order.
+ * So is much more easier to build a tree that deleteing the node from the bottom
+ * Therefore, we need to use postorder traversal.
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
  * */
 class Solution
 {
@@ -23,15 +27,21 @@ class Solution
     TreeNode *removeLeafNodes(TreeNode *root, int target)
     {
         if (!root)
+        {
             return nullptr;
+        }
 
-        root->left = removeLeafNodes(root->left, target);
-        root->right = removeLeafNodes(root->right, target);
+        TreeNode *left = removeLeafNodes(root->left, target);
+        TreeNode *right = removeLeafNodes(root->right, target);
 
-        // Delete the node
-        // make sure is leaf
-        if (!root->left && !root->right && root->val == target)
+        // Make sure is a leaf node
+        if (left == nullptr && right == nullptr && root->val == target)
+        {
             return nullptr;
+        }
+
+        root->left = left;
+        root->right = right;
 
         return root;
     }
