e6991f6b962fe6386f82adbea2509837725dd7bc
linnsheng <linnsheng2002@gmail.com>
2026-02-10 11:00:50 +0800

    daily

--- 49.Group_Anagrams.cpp
+++ 49.Group_Anagrams.cpp
@@ -13,6 +13,9 @@
  * The brute force way will be just simply loop through the array and check whether each string has the same number of
  * character with the string or not. But this is very inefficient. Time Complexity: O(n^3)
  *
+ * What makes an anagrams is that each string has the same number of alphabets. So storing the number of alphabets in
+ * each string is the key
+ *
  * Are we able to furhter optimize it by maybe reducing the number of loop? Because to search thorugh the character is a
  * must action and cannot be avoidable. What we can do is has a hash map to store a vector of anagrams string. Since
  * anagram has the same number of character for every single character, then if we sort them then they must be the same
@@ -28,21 +31,26 @@ class Solution
   public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs)
     {
-        std::vector<std::vector<std::string>> answer;
-
         std::unordered_map<std::string, std::vector<std::string>> map;
 
         for (const auto &str : strs)
         {
-            std::string temp{str};
-            // sort the string
-            std::sort(temp.begin(), temp.end());
+            // Building our own key
+            std::vector<int> freq(26, 0);
 
-            map[temp].emplace_back(str);
+            for (const auto &ch : str)
+                freq[ch - 'a']++;
+
+            std::string key{};
+            for (const auto &i : freq)
+                key += "#" + std::to_string(i);
+
+            map[key].push_back(str);
         }
 
-        for (const auto &[_, vec] : map)
-            answer.emplace_back(vec);
+        std::vector<std::vector<std::string>> answer;
+        for (const auto &[_, strs] : map)
+            answer.emplace_back(strs);
 
         return answer;
     }
