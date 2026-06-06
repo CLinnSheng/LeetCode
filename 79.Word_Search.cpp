b4e272f1f1dcd78b165827493407a232ece68508
linnsheng <linnsheng2002@gmail.com>
2026-06-06 15:44:25 +0800

    lc

--- 79.Word_Search.cpp
+++ 79.Word_Search.cpp
@@ -4,63 +4,71 @@
 #include <vector>
 
 /*
- * Goal: Return true if the word is present in the grid otherwise return false
+ * Check whether we can find the word in the board or not
+ * So this is also a decision tree problem at every index we choose it and then we check whether does it matches with
+ * the current index, if yes we continue to saerch otherwise we backtrack and try other dircetion
+ * So from here we know that backtrack is the algo to use
  *
- * Intuition:
- * We can use dfs and backtracking. DFS on every index
- * Time Complexity: O(n^2)
+ * At each cell we have 4 choices/directions to go
+ * Time Complexity: O(m * 4^n) where m is the total number of cells
+ * Space Complexity: O(n)
  * */
+
 class Solution
 {
-  private:
-    const std::vector<std::pair<int, int>> DIRECTIONS{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    const std::vector<std::pair<int, int>> DIRECTIONS{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
   public:
     bool exist(std::vector<std::vector<char>> &board, std::string word)
     {
         int ROW(board.size()), COL(board[0].size());
+
+        // Track which cell we visited prevent looping
         std::vector<std::vector<bool>> visited(ROW, std::vector<bool>(COL, false));
 
-        if (word.length() == 0)
-            return true;
-
-        std::function<bool(const int &, const int &, const int &)> dfs_backtracking =
-            [&](const int &row, const int &col, const int &index) {
-                // Mark it first
-                visited[row][col] = true;
-
-                if (index < word.length() && board[row][col] != word[index])
-                    return false;
-
-                if (index == word.length() - 1)
-                    return true;
-
-                for (const auto &direction : DIRECTIONS)
-                {
-                    int newRow{direction.first + row};
-                    int newCol{direction.second + col};
-
-                    if (newRow < 0 || newCol < 0 || newRow >= ROW || newCol >= COL || visited[newRow][newCol])
-                        continue;
-
-                    if (dfs_backtracking(newRow, newCol, index + 1))
-                        return true;
-
-                    // false
-                    // backtrack to try other path
-                    visited[newRow][newCol] = false;
-                }
-
-                return false;
-            };
-
-        for (int r{}; r < ROW; r++)
-            for (int c{}; c < COL; c++)
+        std::function<bool(const int, const int, const int)> backtrack = [&](const int index, const int row,
+                                                                             const int col) {
+            // Base Case
+            if (index == word.length())
             {
-                if (dfs_backtracking(r, c, 0))
-                    return true;
-                visited[r][c] = false;
+                return true;
             }
+
+            if (row < 0 || col < 0 || row >= ROW || col >= COL || visited[row][col] || word[index] != board[row][col])
+            {
+                return false;
+            }
+
+            visited[row][col] = true;
+
+            for (const auto &[x, y] : DIRECTIONS)
+            {
+                int new_row = row + x;
+                int new_col = col + y;
+
+                if (backtrack(index + 1, new_row, new_col))
+                {
+                    return true;
+                }
+            }
+
+            // Backtrack
+            visited[row][col] = false;
+            return false;
+        };
+
+        // Try every single cell as the starting
+        for (int row{}; row < ROW; row++)
+        {
+            for (int col{}; col < COL; col++)
+            {
+                if (backtrack(0, row, col))
+                {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 };
