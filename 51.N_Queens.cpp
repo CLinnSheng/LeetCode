e78a88b40d43d1fae98dd5ac934fbcf838f85cff
linnsheng <linnsheng2002@gmail.com>
2026-06-12 06:55:12 +0800

    lc

--- 51.N_Queens.cpp
+++ 51.N_Queens.cpp
@@ -4,56 +4,64 @@
 #include <vector>
 
 /*
- * Goal: Return all distinct solution to the n-queens puzzle
+ * Return all the possible solution to place n queens in a NxN board.
+ * So in another way, it also means what is all the possible path.
+ * Since finding all the possible path --> Backtracking
+ * But we need to take care of how can we place the queen
+ * Queen can go horizontally, vertically and diagonally.
+ * So before placing each pieces we need to check every one of it
+ * We can choose either iterate the rows/columns but it doesnt matter
+ * The problem here is how can we check the diagonal or what value should we check if we store the placed queens
+ * in a set or array.
+ * If we take rows --> Then we only care about the `upper` left & right diagonal
+ * Notice that (0,0) , (1,1), (2,2) & ... is the same
+ * So we can come up with row - col and also row + col for each diagonal
  *
- * Intuition:
- * This is actually a tree decision problem, at each cell we can choose to place the queen or not and then keep dfs
- * Once we reach a state where we cannot proceed we can backtrack to try other cell.
- * Time Complexity: O(n^2)
+ * Time Complexity: O(n!)
+ * Because at every row we only left with n - 1 column to place
+ * Space Complexity: O(n^2)
  * */
+
 class Solution
 {
   public:
     std::vector<std::vector<std::string>> solveNQueens(int n)
     {
-        if (!n)
-            return {};
+        std::vector<std::vector<std::string>> ans;
+        std::vector<std::string> temp(n, std::string(n, '.'));
+        std::unordered_set<int> cols, left_diagonals, right_diagonals;
 
-        // A hash set to track the placed and restricted region
-        std::unordered_set<int> cols, diagonal1, diagonal2;
-        std::vector<std::string> steps(n, std::string(n, '.'));
-        std::vector<std::vector<std::string>> answer;
-
-        std::function<void(const int &)> dfs_backtracking = [&](const int &row) {
+        std::function<void(const int)> backtracking = [&](const int row) {
             if (row == n)
             {
-                answer.emplace_back(steps);
+                ans.push_back(temp);
                 return;
             }
 
-            // Iterating through the columns of the specific row
-            for (int c{}; c < n; c++)
+            // Try all possible columns
+            for (int col{}; col < n; col++)
             {
-                // Checking
-                if (cols.count(c) == 0 && diagonal1.count(c + row) == 0 && diagonal2.count(c - row) == 0)
+                // Check whether is it safe to place or not
+                if (!(cols.count(col) || left_diagonals.count(row - col) || right_diagonals.count(row + col)))
                 {
-                    cols.insert(c);
-                    // 2 direction of diagonal
-                    diagonal1.insert(c + row);
-                    diagonal2.insert(c - row);
-                    steps[row][c] = 'Q';
+                    // Place it
+                    cols.insert(col);
+                    left_diagonals.insert(row - col);
+                    right_diagonals.insert(row + col);
+                    temp[row][col] = 'Q';
 
-                    dfs_backtracking(row + 1);
+                    backtracking(row + 1);
 
-                    // backtracking
-                    cols.erase(c);
-                    diagonal1.erase(c + row);
-                    diagonal2.erase(c - row);
-                    steps[row][c] = '.';
+                    // Backtrack
+                    cols.erase(col);
+                    left_diagonals.erase(row - col);
+                    right_diagonals.erase(row + col);
+                    temp[row][col] = '.';
                 }
             }
         };
-        dfs_backtracking(0);
-        return answer;
+
+        backtracking(0);
+        return ans;
     }
 };
