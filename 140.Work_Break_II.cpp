e4fa149b0968bc7bf74ed21ca074d9c87c3e47ea
linnsheng <linnsheng2002@gmail.com>
2026-06-13 11:30:46 +0800

    lc

--- 140.Work_Break_II.cpp
+++ 140.Work_Break_II.cpp
@@ -4,46 +4,53 @@
 #include <vector>
 
 /*
- * Add space in s sto construt a sentence where each word is a valid dictionary word
- * Goal: Return all possible sentences in any order
+ * Add space in s to construct a sentence where each word is a valid dictionary word
+ * So we are trying to split the string s such that each word is a valid dictionary word
+ * First think to efficeintly check whether is it a valid dicitionary word.
+ * Lets store the wordDict in a hashset instead -> O(1)
+ * We want to find all the possible combination --> Backtracking
+ * At each index we will determine want to split or not by checking is it a valid word
  *
- * Intuition:
- * This is also actually a tree dicision problem, at every index we determine whether is this a valid word or not. If it
- * is we will continue to dfs or else we will just backtrack until we reach the last index For checking we can use a
- * hash set so we can achieve in O(1)
- * Time Complexity: O(n * 2^n) --> n is due to the building of the substr
+ * Time Complexity: O(n * 2^n)
+ * 2^n possible combination because at each index we can either choose to split or not
+ * And then for each combination we will do a linear time of creating the substr
+ * Space Complexity: O(2^n)
  * */
+
 class Solution
 {
   public:
     std::vector<std::string> wordBreak(std::string s, std::vector<std::string> &wordDict)
     {
-        std::unordered_set<std::string> dict(wordDict.begin(), wordDict.end());
-        std::vector<std::string> answer;
+        std::unordered_set<std::string> words(wordDict.begin(), wordDict.end());
+        std::vector<std::string> ans;
+        std::string currStr{};
 
-        std::function<void(const int &, const std::string)> dfs_backtracking = [&](const int &index,
-                                                                                   const std::string currStr) {
+        std::function<void(const int)> backtracking = [&](const int index) {
             if (index == s.length())
             {
-                answer.emplace_back(currStr);
+                ans.push_back(currStr);
                 return;
             }
 
             for (int i{index}; i < s.length(); i++)
             {
-                std::string subStr{s.substr(index, i - index + 1)};
+                // O(n)
+                std::string substr = s.substr(index, i - index + 1);
 
-                // Checking
-                if (dict.find(subStr) != dict.end())
+                if (words.count(substr))
                 {
-                    if (currStr.size())
-                        dfs_backtracking(i + 1, currStr + " " + subStr);
-                    else
-                        dfs_backtracking(i + 1, subStr);
+                    auto prev = currStr;
+
+                    currStr += currStr.empty() ? substr : " " + substr;
+                    backtracking(i + 1);
+
+                    //  backtracking
+                    currStr = prev;
                 }
             }
         };
-        dfs_backtracking(0, "");
-        return answer;
+        backtracking(0);
+        return ans;
     }
 };
