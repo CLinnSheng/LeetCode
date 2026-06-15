493e69a8cfe3e268fdb5636c7d25b107e1242c67
linnsheng <linnsheng2002@gmail.com>
2026-06-15 10:29:14 +0800

    lc

--- 2707.Extra_Characters_in_a_String.cpp
+++ 2707.Extra_Characters_in_a_String.cpp
@@ -1,65 +1,57 @@
 #include <algorithm>
+#include <array>
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-
 /*
- * Given a string s & a dictionary of words dictionary. Break s into one or more non-empty overlapping substrings such
- * taht each substring is present in dictionary There may be some extra characters in s which is not present in the
- * dictionary
+ * Return the minimum number of extra characters left over if break up S optimally
+ * Problme here is how can we break up S optimally? Brute force way will be trying all possible way of breaking up the S
+ * Store the dicitionary in a set
+ * Time Complexity:
+ * Each index of the string is computed once O(n), and for each function it iterate thorugh the string and created
+ * substring here is O(n^2) Hence, O(n^3)
  *
- * Goal: Return the minimum number of extra characters left over after breakking up S optimally
- *
- * Intuition:
- * We need to break s optimally such that it will has lesser extra character
- * One important thing is that we can actually skip character while breaking up the s.
- *
- * So for brute force
- * We will just have to try every single substring from length of 1
- * We can use a hash set to store the words so we can access it in O(1)
- * We can optimize it by caching because we repeatedly solving the same subproblem multiple time
- * Time Complexity: O(n^3)
- *
- * We can actually further optimize it at the substr over there.
- * If you notice current caching method, it will have to keep check for every substr. Why not we use the first character
- * as a determination point? By checking through the prefix? So we can straight away break if the first few characters
- * is not in our data structure
- * eg: [leet, code, leetcode]. s = "abcasdsdsdsds". Since a is not in the prefix of any words so we can skip the
- * character
- * Time complexity: O(n^2 + m * k)
+ * Can we further optimize it? Do we really need to create substr which cost O(n) while iterate thorugh the str?
+ * How can i check whether the substr exist without creating it? From here we can think of using a Trie Structure
  * */
 
 struct TrieNode
 {
-    std::unordered_map<int, TrieNode *> alphabets;
-    bool isWord;
+    std::array<std::shared_ptr<TrieNode>, 26> children;
+    bool isWord{};
 
     TrieNode() : isWord(false)
     {
+        std::fill(children.begin(), children.end(), nullptr);
     }
 };
 
 struct TrieTree
 {
-    TrieNode *root;
-    TrieTree(const std::vector<std::string> &words)
+    std::shared_ptr<TrieNode> root;
+
+    TrieTree()
     {
-        root = new TrieNode();
+        root = std::make_shared<TrieNode>();
+    }
 
-        for (const auto &word : words)
+    void insert(const std::string &str)
+    {
+        auto currNode = root;
+
+        for (const auto ch : str)
         {
-            auto temp{root};
-            for (const auto &ch : word)
+            if (currNode->children[ch - 'a'] == nullptr)
             {
-                if (temp->alphabets.find(ch - 'a') == temp->alphabets.end())
-                    temp->alphabets[ch - 'a'] = new TrieNode();
-
-                temp = temp->alphabets[ch - 'a'];
+                currNode->children[ch - 'a'] = std::make_shared<TrieNode>();
             }
 
-            temp->isWord = true;
+            currNode = currNode->children[ch - 'a'];
         }
+
+        currNode->isWord = true;
     }
 };
 
@@ -67,53 +59,78 @@ class Solution
 {
   private:
     std::unordered_map<int, int> cache;
-    TrieTree *tree;
+    std::unordered_set<std::string> set;
+    std::shared_ptr<TrieTree> tree;
 
-    // O(n)
-    int bruteForce(const int &index, const std::string &s, const std::unordered_set<std::string> &sets)
+    int solution_1(const std::string &s, const int index)
     {
+        // We can further optimize it because if you notice we keep calling this function with the same index
+
         // Base Case
         if (index == s.length())
-            return 0;
-
-        if (cache.count(index))
-            return cache[index];
-
-        // Trying out every single substring from length of 1, skipping this character
-        int ans = 1 + bruteForce(index + 1, s, sets);
-
-        for (int i{index}; i < s.length(); i++) // O(n)
         {
-            // if this substring exists, then dfs on the next index
-            if (sets.count(s.substr(index, i - index + 1))) // O(n)
-                ans = std::min(ans, bruteForce(i + 1, s, sets));
+            return 0;
+        }
+
+        if (cache.find(index) != cache.end())
+        {
+            return cache[index];
+        }
+
+        // Skipping this character
+        int ans = 1 + solution_1(s, index + 1);
+
+        // O(n)
+        for (int i{index}; i < s.length(); i++)
+        {
+            // O(n)
+            auto subStr = s.substr(index, i - index + 1);
+
+            if (set.find(subStr) != set.end())
+            {
+                // Split over here
+                ans = std::min(ans, solution_1(s, i + 1));
+            }
         }
 
         return cache[index] = ans;
     }
 
-    int dfs_trie(const int &index, const std::string &s)
+    int solution_2(const std::string &s, const int index)
     {
+        // We can further optimize it because if you notice we keep calling this function with the same index
+
         // Base Case
         if (index == s.length())
-            return 0;
-
-        if (cache.count(index))
-            return cache[index];
-
-        // Trying out every single substring from length of 1, skipping this character
-        int ans = 1 + dfs_trie(index + 1, s);
-        TrieNode *currNode = tree->root;
-
-        for (int i{index}; i < s.length(); i++) // O(n)
         {
-            if (currNode->alphabets.find(s[i] - 'a') == currNode->alphabets.end())
-                break;
+            return 0;
+        }
 
-            currNode = currNode->alphabets[s[i] - 'a'];
+        if (cache.find(index) != cache.end())
+        {
+            return cache[index];
+        }
+
+        // Skipping this character
+        int ans = 1 + solution_2(s, index + 1);
+
+        auto currNode = tree->root;
+
+        // O(n)
+        for (int i{index}; i < s.length(); i++)
+        {
+            if (currNode->children[s[i] - 'a'] == nullptr)
+            {
+                break;
+            }
+
+            currNode = currNode->children[s[i] - 'a'];
 
             if (currNode->isWord)
-                ans = std::min(ans, dfs_trie(i + 1, s));
+            {
+                // Split over here
+                ans = std::min(ans, solution_2(s, i + 1));
+            }
         }
 
         return cache[index] = ans;
@@ -122,11 +139,14 @@ class Solution
   public:
     int minExtraChar(std::string s, std::vector<std::string> &dictionary)
     {
-        // Caching: O(n^3)
-        // std::unordered_set<std::string> sets(dictionary.begin(), dictionary.end());
-        // return bruteForce(0, s, sets);
+        // this->set = std::unordered_set<std::string>(dictionary.begin(), dictionary.end());
+        // return solution_1(s, 0);
+        this->tree = std::make_shared<TrieTree>();
+        for (const auto word : dictionary)
+        {
+            tree->insert(word);
+        }
 
-        tree = new TrieTree(dictionary);
-        return dfs_trie(0, s);
+        return solution_2(s, 0);
     }
 };
