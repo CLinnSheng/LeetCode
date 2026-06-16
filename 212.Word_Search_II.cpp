7bbdcf5e4005e5b3de7c6c2716e0be7d2503fd3b
linnsheng <linnsheng2002@gmail.com>
2026-06-16 16:51:51 +0800

    lc

--- 212.Word_Search_II.cpp
+++ 212.Word_Search_II.cpp
@@ -1,6 +1,8 @@
+#include <algorithm>
+#include <array>
 #include <functional>
+#include <memory>
 #include <string>
-#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
@@ -13,30 +15,43 @@
  * We can use dfs to search the grid and backtracking when the string form doesnt match any of the prefix of the words.
  * The time complexity will be: O(N^2 * m^2) the m^2 is iterating through the words and check whether got this prefix or
  * not We can optimize this part by using a Trie Tree data structure to solve checking prefix
+ * Trie Tree can optimize the checking of string by passing the currNode and check its flag
  * */
 
 struct TrieNode
 {
-    std::unordered_map<int, TrieNode *> alphabets;
-    bool isWord;
+    std::array<std::shared_ptr<TrieNode>, 26> children;
+    bool isWord{};
 
     TrieNode() : isWord(false)
     {
+        std::fill(children.begin(), children.end(), nullptr);
+    }
+};
+
+struct TrieTree
+{
+    std::shared_ptr<TrieNode> root;
+
+    TrieTree()
+    {
+        root = std::make_shared<TrieNode>();
     }
 
-    void addWord(const std::string &str)
+    void insert(const std::string &str)
     {
-        auto temp{this};
+        auto currNode = root;
 
-        for (const auto &ch : str)
+        for (const auto ch : str)
         {
-            if (temp->alphabets.find(ch - 'a') == temp->alphabets.end())
-                temp->alphabets[ch - 'a'] = new TrieNode();
-
-            temp = temp->alphabets[ch - 'a'];
+            if (currNode->children[ch - 'a'] == nullptr)
+            {
+                currNode->children[ch - 'a'] = std::make_shared<TrieNode>();
+            }
+            currNode = currNode->children[ch - 'a'];
         }
 
-        temp->isWord = true;
+        currNode->isWord = true;
     }
 };
 class Solution
@@ -47,43 +62,51 @@ class Solution
   public:
     std::vector<std::string> findWords(std::vector<std::vector<char>> &board, std::vector<std::string> &words)
     {
-        std::unordered_set<std::string> answer;
-        if (words.size() == 0)
-            return {};
-
-        int ROWS(board.size()), COLS(board[0].size());
+        std::shared_ptr<TrieTree> tree = std::make_shared<TrieTree>();
+        std::unordered_set<std::string> ans;
+        int ROWS = board.size(), COLS = board[0].size();
         std::vector<std::vector<bool>> visited(ROWS, std::vector<bool>(COLS, false));
 
-        TrieNode *root = new TrieNode();
-        for (const auto &word : words)
-            root->addWord(word);
+        for (const auto &str : words)
+        {
+            tree->insert(str);
+        }
 
-        std::function<void(const int &, const int &, TrieNode *, std::string)> dfs_backtracking =
-            [&](const int &row, const int &col, TrieNode *currNode, std::string word) {
+        std::function<void(const int, const int, std::shared_ptr<TrieNode>, const std::string &)> backtracking =
+            [&](const int row, const int col, std::shared_ptr<TrieNode> node, std::string currStr) {
                 if (row < 0 || col < 0 || row >= ROWS || col >= COLS || visited[row][col] ||
-                    currNode->alphabets[board[row][col] - 'a'] == nullptr)
-                    return;
-
-                visited[row][col] = true;
-                word += board[row][col];
-                currNode = currNode->alphabets[board[row][col] - 'a'];
-
-                if (currNode->isWord)
-                    answer.insert(word);
-
-                for (const auto &direction : DIRECTIONS)
+                    node->children[board[row][col] - 'a'] == nullptr)
                 {
-                    int newRow{direction.first + row}, newCol{direction.second + col};
-
-                    dfs_backtracking(newRow, newCol, currNode, word);
+                    return;
                 }
 
+                // Include this cell after boundary checking
+                currStr += board[row][col];
+                visited[row][col] = true;
+                node = node->children[board[row][col] - 'a'];
+
+                if (node->isWord)
+                {
+                    ans.insert(currStr);
+                }
+
+                for (const auto &[y, x] : DIRECTIONS)
+                {
+                    backtracking(row + y, col + x, node, currStr);
+                }
+
+                // Backtrack
                 visited[row][col] = false;
             };
 
-        for (int i{}; i < ROWS; i++)
-            for (int j{}; j < COLS; j++)
-                dfs_backtracking(i, j, root, "");
-        return std::vector<std::string>(answer.begin(), answer.end());
+        // Try start from evey single cell
+        for (int row{}; row < ROWS; row++)
+        {
+            for (int col{}; col < COLS; col++)
+            {
+                backtracking(row, col, tree->root, "");
+            }
+        }
+        return std::vector<std::string>(ans.begin(), ans.end());
     }
 };
--- 463.Island_Perimeter.cpp
+++ 463.Island_Perimeter.cpp
@@ -9,42 +9,58 @@
  * Intuition:
  * This is a grpah problem so we can simply use dfs
  * Since there is only one island we just have to dfs once. And we use an array to keep track of the cell we visited
+ * And how do we handle the perimeter? If we notice the exmaple given the perimeter is only added when it is out of
+ * bound
  *
  * Time Complexity: O(m * n)
  * */
 class Solution
 {
   private:
-    std::vector<std::pair<int, int>> DIRECTIONS{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    const std::vector<std::pair<int, int>> DIRECTIONS{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
   public:
     int islandPerimeter(std::vector<std::vector<int>> &grid)
     {
-        int ROWS(grid.size()), COLS(grid[0].size());
+        int perimeter{};
+        int ROWS = grid.size(), COLS = grid[0].size();
         std::vector<std::vector<bool>> visited(ROWS, std::vector<bool>(COLS, false));
 
-        std::function<int(const int &, const int &)> dfs = [&](const int &row, const int &col) {
-            // Base Case, cannot traverse only return the side length other wise is 0
+        std::function<int(const int, const int)> dfs = [&](const int row, const int col) {
+            // Check boundary
             if (row < 0 || col < 0 || row >= ROWS || col >= COLS || grid[row][col] == 0)
+            {
                 return 1;
+            }
 
             if (visited[row][col])
+            {
                 return 0;
+            }
 
-            // Mark as visited
+            // mark visit
             visited[row][col] = true;
 
-            int answer{};
+            int perimeter{};
 
-            for (const auto &direction : DIRECTIONS)
-                answer += dfs(row + direction.first, col + direction.second);
+            for (const auto direction : DIRECTIONS)
+            {
+                perimeter += dfs(row + direction.first, col + direction.second);
+            }
 
-            return answer;
+            return perimeter;
         };
-        for (int i{}; i < ROWS; i++)
-            for (int j{}; j < COLS; j++)
-                if (grid[i][j])
-                    return dfs(i, j);
+
+        for (int row{}; row < ROWS; row++)
+        {
+            for (int col{}; col < COLS; col++)
+            {
+                if (grid[row][col])
+                {
+                    return dfs(row, col);
+                }
+            }
+        }
         return -1;
     }
 };
